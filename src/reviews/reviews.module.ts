51cc40604918b6f138a0aeecde9ed476b0fa7a42
brunafreitasds <bruna.freitas@furg.br>
2022-06-26 17:36:51 -0300

    add module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -3,11 +3,11 @@ import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Reviews } from './entities/reviews.entity';
+import { Review } from './entities/reviews.entity';
 import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reviews, Media , User])],
+  imports: [TypeOrmModule.forFeature([Review, Media , User])],
   controllers: [ReviewsController],
   providers: [ReviewsService]
 })
